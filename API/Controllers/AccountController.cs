e4368c5f9f1a48322fa56ca86108ac3c83a18f8e
Aashik Kore <aashikkore07@gmail.com>
2025-08-18 00:57:49 +0530

    Added security with hashing paswwprd with saltpassword and secured communication with server aloing with JWT Authentication

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
+{
+    [HttpPost("Register")] // apo/account/register
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        if (await EmailExists(registerDto.Email)) return BadRequest("User Already Exists!!");
+        var hmac = new HMACSHA512();
+
+        var user = new AppUser()
+        {
+            Email = registerDto.Email,
+            DisplayName = registerDto.DisplayName,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+         return new UserDto()
+         {
+             Id = user.Id,
+             DisplayName = user.DisplayName,
+             Email = user.Email,
+             Token = tokenService.CreateToken(user)
+         };
+    }
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+    }
+
+    [HttpPost("Login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(x => x.Email.ToLower() == loginDto.Email.ToLower());
+
+        if (user == null) return Unauthorized("Invalid Email Address!!");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+        for (var i = 0; i < computeHash.Length; i++)
+        {
+            if(computeHash[i] != user.PasswordHash[i]) return Unauthorized("Passwprd Incorrect");
+        }
+        
+         return new UserDto()
+         {
+             Id = user.Id,
+             DisplayName = user.DisplayName,
+             Email = user.Email,
+             Token = tokenService.CreateToken(user)
+         };
+
+    }
+}
